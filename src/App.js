4508a2fdb9ffa850561d823e0ee5b5c2e33896f1
unknown <haasin.farooq@gmail.com>
2020-11-16 01:54:13 +0500

    complete functionalities

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import TodoList from './components/TodoList';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="todo-app">
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const TodoForm = (props) => {
+    const [input, setInput] = useState('');
+
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random() * 10000),
+            text: input
+        })
+
+        setInput('');
+    }
+
+    return (
+        <form className="todo-form" onSubmit={handleSubmit}>
+            <input className="todo-input" type="text" placeholder="Add a Todo" value={input} name="text" onChange={handleChange}/>
+            <button className="todo-button">Add Todo</button>
+        </form>
+    )
+}
+
+export default TodoForm;
